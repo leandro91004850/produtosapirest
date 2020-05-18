f9b7d630d951b00c360fa8419722d0d9cd08b1ec
Leandro de Almeida <leandroleekrap@gmail.com>
2020-05-17 22:50:23 -0300

    criando e configurando DB Postgres e criando model

---
+++ src/main/java/com/produtos/apirest/respository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.produtos.apirest.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.produtos.apirest.models.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
