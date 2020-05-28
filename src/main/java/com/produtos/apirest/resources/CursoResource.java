b4f71189f36a08cfd997a4565507267f60522e61
Leandro de Almeida <leandroleekrap@gmail.com>
2020-05-27 23:03:34 -0300

    ajustes no CRUD

--- src/main/java/com/produtos/apirest/resources/CursoResource.java
+++ src/main/java/com/produtos/apirest/resources/CursoResource.java
@@ -2,6 +2,10 @@ package com.produtos.apirest.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +29,19 @@ public class CursoResource {
 		Iterable<Cursos> curso = servico.findAll();
 		return ResponseEntity.ok().body(curso);		
 	
+	}
+	
+	// retorna um unico id
+	@GetMapping("/cursos/{id}")
+	public Cursos listaUnicoCurso(@PathVariable(value="id") int id) {
+		return servico.findById(id);
+		
+	}
+	
+	//salva cursos
+	@PostMapping("/cursos")
+	public Cursos salvarProduto(@RequestBody Cursos cursos) {
+		return servico.save(cursos);
 	}
 	
 }
--- src/main/java/com/produtos/apirest/respository/CursoRepository.java
+++ src/main/java/com/produtos/apirest/respository/CursoRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.produtos.apirest.models.Cursos;
 
 public interface CursoRepository extends JpaRepository<Cursos, Integer> {
+
+	Cursos save(Integer id);
+	Cursos findById(int id);
+
 	
 
 
